46e80b67fadb56a28b840e159932666a63120cbf
Uzair Javed <uzairjaved75@gmail.com>
2019-08-28 04:22:29 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let timer = 60;
+let timer = 120;
 let min = 0;
 let sec = 0;
+let a = new Audio();
 
-let ding;                 //AUDIO
-
-function preload() {
-    ding = dingAudio.play();
-}
 function backFunction() {
     window.history.back();      // For back page
 }
+
+let upgradeTime = function () {
+    startTimer();
+}
+
 
 let dingAudio = document.getElementById("#myAudio");
 
@@ -17,19 +18,18 @@ let startTimer = () => {
     min = parseInt(timer / 60);
     sec = parseInt(timer % 60);
 
-    if (timer <= 0) {
+    if (timer <= 0) {                         //popup Page
         window.location = "wakeup.html";
-        ding;
+        a.src = 'ding.mp3';
+        a.play();
     }
 
     document.querySelector("#timer").innerHTML =
         "Time Left : " + "0" + min.toString() + ": " + sec.toString();
     timer--;
 
+    setTimeout(upgradeTime, 1000);
 
-    setTimeout(function () {
-        startTimer();
-    }, 1000);
 
 }
 
